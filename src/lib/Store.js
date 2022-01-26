c81c4f270a02868358d9e16bb6d116a8f16ff4e1
Anneke Kleppe <anneke@warmerkleppe.nl>
2022-01-26 18:12:19 +0100

    removed accidentally created .js files

--- src/lib/Store.js
+++
@@ -1,8 +0,0 @@
-import { writable } from 'svelte/store';
-// info about ProjectIt
-export const versionNumber = "0.2.0";
-export const miniWindow = writable(false);
-export const leftPanelVisible = writable(false);
-// a global ==> hack instead of using svg for projectit logo
-export const darkMode = writable(false);
-//# sourceMappingURL=Store.js.map
