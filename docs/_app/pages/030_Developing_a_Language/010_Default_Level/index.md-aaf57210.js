31a18742e3da12724fbab668fc7ad68694ee9e07
Anneke Kleppe <anneke@warmerkleppe.nl>
2022-07-17 11:20:50 +0200

    removed generated files

--- docs/_app/pages/030_Developing_a_Language/010_Default_Level/index.md-aaf57210.js
+++
@@ -1,1 +0,0 @@
-import{S as t,i as a,s as n}from"../../../chunks/vendor-492729b6.js";async function l(){return{status:302,redirect:"/030_Developing_a_Language/010_Default_Level/010_Defining_the_Language_Structure"}}class _ extends t{constructor(e){super();a(this,e,null,null,n,{})}}export{_ as default,l as load};
